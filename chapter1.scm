7cce8d97dadfe5a0fc32fa039d161370f4755def
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-04-18 15:49:49 +0300

    Update excercises from chapter 1

--- chapter1.scm
+++ chapter1.scm
@@ -25,3 +25,26 @@
    (* 3 (- 6 2) (- 2 7)))
 
 ;; Exercise 1.3
+(define (square x) (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(define (sum-of-squares-of-two-larger-numbers a b c)
+  (cond ((and (>= a c) (>= b c)) (sum-of-squares a b))
+        ((and (>= a b) (>= c b)) (sum-of-squares a c))
+        ((and (>= b a) (>= c a)) (sum-of-squares b c))))
+
+(sum-of-squares-of-two-larger-numbers 4 3 2)
+(sum-of-squares-of-two-larger-numbers 3 2 4)
+(sum-of-squares-of-two-larger-numbers 2 3 4)
+(sum-of-squares-of-two-larger-numbers 4 4 2)
+
+;; Exercise 1.4
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+(a-plus-abs-b 3 -4)
+(a-plus-abs-b 11 4)
+
+;; Exercise 1.5
